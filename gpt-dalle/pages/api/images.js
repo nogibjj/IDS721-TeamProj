988e45a43905fd5eacc1ce88d60ca07c8344e76c
hugohu <0>
2023-04-20 14:20:19 -0400

    Initial Next.js framework

---
+++ gpt-dalle/pages/api/images.js
@@ -0,0 +1,15 @@
+export default async function handler(req, res) {
+  const { Configuration, OpenAIApi } = require("openai");
+  const configuration = new Configuration({
+    apiKey: "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"
+  });
+  const openai = new OpenAIApi(configuration);
+  const response = await openai.createImage({
+    prompt: req.query.p,
+    n: parseInt(req.query.n),
+    size: "1024x1024",
+  });
+  console.log(response.data.data);
+  res.status(200).json({ result: response.data.data })
+}
+
