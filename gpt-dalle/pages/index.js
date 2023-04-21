c54d2b0d752c7dbfa1a9a5853d0ab8afc20419eb
Bryce Shi <es474@duke.edu>
2023-04-21 09:52:39 -0400

    replace image placeholder with real ones

--- gpt-dalle/pages/index.js
+++ gpt-dalle/pages/index.js
@@ -22,7 +22,6 @@ export default function Home() {
   const elementRef = useRef(null);
   const textRef = useRef(null);
 
-
   function getImages() {
     if (prompt != "") {
       setError(false);
@@ -69,12 +68,12 @@ export default function Home() {
             placeholder="Prompt"
           />
           {"  "}
-          {results.length == 0 ? (
+          {results.length > 0 ? (
             <button onClick={edit}>Edit</button>
           ) : (
             <button onClick={getImages}>Get Images</button>
           )}
-        </p >
+        </p>
         <small>
           Picture Ratio:&nbsp;{" "}
           <select
@@ -87,52 +86,63 @@ export default function Home() {
             <option value="classic">3:2 Classic</option>
             <option value="square">1:1 Square</option>
             <option value="protrait">3:4 Protrait</option>
-          </select>
-          {" "}
+          </select>{" "}
         </small>
         <br />
-        {error ? (<div className={styles.error}>Something went wrong. Try again.</div>) : (<></>)}
-        {loading && (<div className={styles.loading}>Loading...</div>)}
+        {error ? (
+          <div className={styles.error}>Something went wrong. Try again.</div>
+        ) : (
+          <></>
+        )}
+        {loading && <div className={styles.loading}>Loading...</div>}
         <div className={styles.grid}>
-          {/* {results.map((result) => { */}
-          {/* return ( */}
-          <div className={styles.card}>
-
-            <img ref={elementRef} className={styles.imgPreview} src="https://hips.hearstapps.com/hmg-prod/images/little-cute-maltipoo-puppy-royalty-free-image-1652926025.jpg?crop=0.444xw:1.00xh;0.129xw,0&resize=980:*" />
-            {editing && (
-              <>
-                <Draggable
-                  onDrag={(e) => {
-                    const imgRect = elementRef.current.getBoundingClientRect();
-                    const textRect = textRef.current.getBoundingClientRect();
-                    const relativeX = textRect.left - imgRect.left;
-                    const relativeY = textRect.top - imgRect.top;
-                    setDeltaX(relativeX);
-                    setDeltaY(relativeY);
-                    console.log('Relative X:', deltaX);
-                    console.log('Relative Y:', deltaY);
-                  }}
-                >
-                  <Resizable
-                    defaultSize={{width: boxWidth, height: boxHeight}}         
-                    onResizeStop={(e, direction, ref, d) => {
-                      setBoxWidth(ref.style.width);
-                      setBoxHeight(ref.style.height);
-                      console.log('Box Width:', boxWidth);
-                      console.log('Box Height:', boxHeight);
-                    }}
-                    style={{backgroundColor: "red", display: "flex", position: "absolute", marginTop: "-100px"}}
-                  >
-                    <div ref={textRef}>
-                      Drag me!
-                    </div>
-                  </Resizable>
-                </Draggable>
-              </>
-            )}
-          </div>
-          {/* ); */}
-          {/* })} */}
+          {results.map((result) => {
+            return (
+              <div className={styles.card}>
+                <img
+                  ref={elementRef}
+                  className={styles.imgPreview}
+                  src={result.url}
+                />
+                {editing && (
+                  <>
+                    <Draggable
+                      onDrag={(e) => {
+                        const imgRect =
+                          elementRef.current.getBoundingClientRect();
+                        const textRect =
+                          textRef.current.getBoundingClientRect();
+                        const relativeX = textRect.left - imgRect.left;
+                        const relativeY = textRect.top - imgRect.top;
+                        setDeltaX(relativeX);
+                        setDeltaY(relativeY);
+                        console.log("Relative X:", deltaX);
+                        console.log("Relative Y:", deltaY);
+                      }}
+                    >
+                      <Resizable
+                        defaultSize={{ width: boxWidth, height: boxHeight }}
+                        onResizeStop={(e, direction, ref, d) => {
+                          setBoxWidth(ref.style.width);
+                          setBoxHeight(ref.style.height);
+                          console.log("Box Width:", boxWidth);
+                          console.log("Box Height:", boxHeight);
+                        }}
+                        style={{
+                          backgroundColor: "red",
+                          display: "flex",
+                          position: "absolute",
+                          marginTop: "-100px",
+                        }}
+                      >
+                        <div ref={textRef}>Drag me!</div>
+                      </Resizable>
+                    </Draggable>
+                  </>
+                )}
+              </div>
+            );
+          })}
         </div>
       </main>
     </div>
